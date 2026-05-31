69c756edaea50bdd56fa9fb4c04ea047fee23d3d
moxu <moxu0927@gmail.com>
2026-06-01 02:52:46 +0800

    fix bug

--- index.js
+++ index.js
@@ -866,7 +866,7 @@ async function sendCardPaymentInfo(channel) {
     .setTitle('💳 刷卡付款資訊')
     .setDescription(
       `請點擊以下連結完成刷卡付款：\n\n` +
-      `🔗 付款連結：https://pcpay.tw/aCU67\n\n` +
+      `🔗 PChomePay 合法付款連結：https://pcpay.tw/aCU67\n\n` +
       `付款完成後，請在此頻道上傳付款成功截圖，等待客服確認。\n\n` +
       `截圖請包含：\n` +
       `1. 付款成功畫面\n` +
